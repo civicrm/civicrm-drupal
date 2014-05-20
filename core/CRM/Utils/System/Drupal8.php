ca26ae13f8e60a3f692d3da28832249970e09d40
Torrance <torrance123@gmail.com>
2014-05-21 11:14:00 +1200

    Provide a few comments for CRM_Utils_System_Drupal8::addHTMLHead().

--- core/CRM/Utils/System/Drupal8.php
+++ core/CRM/Utils/System/Drupal8.php
@@ -295,13 +295,18 @@ class CRM_Utils_System_Drupal8 extends CRM_Utils_System_DrupalBase {
    */
   function addHTMLHead($header) {
     static $count = 0;
+
     if (!empty($header)) {
-      $key = 'civi_' . ++$count;
       $data = array(
         '#type' => 'markup',
         '#markup' => $header,
       );
-      drupal_add_html_head($data, $key);
+
+      // The second parameter of drupal_add_html_head() is required to be a unique
+      // key. We simply increment a counter string. This makes it useless for implementations
+      // of hook_html_head_alter() but is required so that we don't clobber previous calls.
+      $count++;
+      drupal_add_html_head($data, "civicrm_{$count}");
     }
   }
 
