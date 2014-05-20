d47113e1c8aea4ce117866d03814a34bb05b1ae0
Torrance <torrance123@gmail.com>
2014-05-21 10:50:00 +1200

    Update CRM_Utils_System_Drupal8::getLoginURL() to Drupal 8.

--- core/CRM/Utils/System/Drupal8.php
+++ core/CRM/Utils/System/Drupal8.php
@@ -221,14 +221,8 @@ class CRM_Utils_System_Drupal8 extends CRM_Utils_System_DrupalBase {
    * @static
    */
   public function getLoginURL($destination = '') {
-    $config = CRM_Core_Config::singleton();
-    $loginURL = $config->userFrameworkBaseURL;
-    $loginURL .= 'user';
-    if (!empty($destination)) {
-      // append destination so user is returned to form they came from after login
-      $loginURL .= '?destination=' . urlencode($destination);
-    }
-    return $loginURL;
+    $query = $destination ? array('destination' => $destination) : array();
+    return \Drupal::url('user.page', array(), array('query' => $query));
   }
 
 
