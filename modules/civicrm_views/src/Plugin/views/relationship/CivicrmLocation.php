e5f107e70376eb78159b3ba19f7dedb90a83983d
Torrance <torrance123@gmail.com>
2014-07-08 12:39:35 +1200

    Heavy development of civicrm_views.

---
+++ modules/civicrm_views/src/Plugin/views/relationship/CivicrmLocation.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Drupal\civicrm_views\Plugin\views\relationship;
+
+use Drupal\views\Plugin\views\relationship\RelationshipPluginBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\views\ViewExecutable;
+use Drupal\views\Plugin\views\display\DisplayPluginBase;
+use Drupal\civicrm\Civicrm;
+
+/**
+ * @ingroup views_relationship_handlers
+ * @ViewsRelationship("civicrm_location")
+ */
+class CivicrmLocation extends RelationshipPluginBase {
+  protected $locations = array();
+  protected $default_location = NULL;
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, Civicrm $civicrm) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->default_location = \CRM_Core_BAO_LocationType::getDefault()->id;
+    $this->locations = \CRM_Core_BAO_Address::buildOptions('location_type_id');
+
+  }
+
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('civicrm')
+    );
+  }
+
+  public function init(ViewExecutable $view, DisplayPluginBase $display, array &$options = NULL) {
+    parent::init($view, $display, $options);
+
+    $this->definition['extra'] = array();
+    if (!empty($this->options['location_type'])) {
+      $this->definition['extra'][] = array(
+        'field' => 'location_type_id',
+        'value' => (int) ($this->options['location_type'] == 'default' ? $this->default_location : $this->options['location_type']),
+        'numeric' => TRUE,
+      );
+    }
+    if (!empty($this->options['is_primary'])) {
+      $this->definition['extra'][] = array(
+        'field' => 'is_primary',
+        'value' => $this->options['is_primary'],
+        'numeric' => TRUE,
+      );
+    }
+    if (!empty($this->options['is_billing'])) {
+      $this->definition['extra'][] = array(
+        'field' => 'is_billing',
+        'value' => $this->options['is_billing'],
+        'numeric' => TRUE,
+      );
+    }
+  }
+
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+    $options['location_type'] = array('default' => 0);
+    $options['is_billing'] = array('default' => FALSE, 'bool' => TRUE);
+    $options['is_primary'] = array('default' => FALSE, 'bool' => TRUE);
+    return $options;
+  }
+
+  public function buildOptionsForm(&$form, &$form_state) {
+    $form['is_primary'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Is primary?'),
+      '#default_value' => isset($this->options['is_primary']) ? $this->options['is_primary'] : FALSE,
+    );
+    $form['is_billing'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Is billing?'),
+      '#default_value' => isset($this->options['is_billing']) ? $this->options['is_billing'] : FALSE,
+    );
+    $form['location_type'] = array(
+      '#type' => 'radios',
+      '#title' => t('Location type'),
+      '#options' => array(
+        0 => t('Any'),
+        'default' => t('Default location (!default)', array('!default' => $this->locations[$this->default_location])),
+      ),
+      '#default_value' => isset($this->options['location_type']) ? (int) $this->options['location_type'] : 0,
+    );
+
+    foreach ($this->locations as $id => $location) {
+      $form['location_type']['#options'][$id] = $location;
+    }
+
+    parent::buildOptionsForm($form, $form_state);
+  }
+}
---
+++ modules/civicrm_views/src/Plugin/views/relationship/CivicrmRelationship.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Drupal\civicrm_views\Plugin\views\relationship;
+
+use Drupal\views\Plugin\views\relationship\RelationshipPluginBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\views\ViewExecutable;
+use Drupal\views\Plugin\views\display\DisplayPluginBase;
+use Drupal\civicrm\Civicrm;
+
+/**
+ * @ingroup views_relationship_handlers
+ * @ViewsRelationship("civicrm_relationship")
+ */
+class CivicrmRelationship extends RelationshipPluginBase {
+  protected $relationships = array();
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, Civicrm $civicrm) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    // relationshipType() returns information about relations as array with fields
+    // 'name_a_b', 'name_b_a', 'contact_type_a' and 'contact_type_b'.
+    $this->relationships[0] = t('Any');
+    foreach (\CRM_Core_PseudoConstant::relationshipType('name') as $id => $value_array) {
+      if ($this->realField == 'contact_id_b') {
+        $this->relationships[$id] = $value_array['name_b_a'];
+      }
+      else {
+        $this->relationships[$id] = $value_array['name_a_b'];
+      }
+    }
+  }
+
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('civicrm')
+    );
+  }
+
+  public function init(ViewExecutable $view, DisplayPluginBase $display, array &$options = NULL) {
+    parent::init($view, $display, $options);
+
+    $this->definition['extra'] = array();
+    if (!empty($this->options['is_active'])) {
+      $this->definition['extra'][] = array(
+        'field' => 'is_active',
+        'value' => TRUE,
+      );
+    }
+    if (!empty($this->options['relationship_type']) && !array_key_exists(0, $this->options['relationship_type'])) {
+      $this->definition['extra'][] = array(
+        'field' => 'relationship_type_id',
+        'value' => $this->options['relationship_type'],
+        'numeric' => TRUE,
+      );
+    }
+  }
+
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+    $options['is_active'] = array('default' => TRUE, 'bool' => TRUE);
+    $options['relationship_type'] = array('default' => 0);
+    return $options;
+  }
+
+  public function buildOptionsForm(&$form, &$form_state) {
+    $form['relationship_type'] = array(
+      '#type' => 'select',
+      '#title' => 'Relationship type',
+      '#multiple' => TRUE,
+      '#options' => $this->relationships,
+      '#default_value' => isset($this->options['relationship_type']) ? $this->options['relationship_type'] : 0,
+      '#required' => TRUE,
+    );
+    $form['is_active'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Ensure CiviCRM relationships are active?'),
+      '#default_value' => isset($this->options['is_active']) ? $this->options['is_active'] : TRUE,
+      '#description' => t('Uncheck this to allow listing of expired or inactive relationships in addition to current relationships.'),
+    );
+
+    parent::buildOptionsForm($form, $form_state);
+  }
+}
