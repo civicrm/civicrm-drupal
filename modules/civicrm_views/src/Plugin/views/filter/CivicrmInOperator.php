fa52fa5edf663a56e34dbd2b4c68d2c4f5246176
Torrance <torrance123@gmail.com>
2014-07-09 12:17:19 +1200

    Remove redundant code from CivicrmInOperator.

--- modules/civicrm_views/src/Plugin/views/filter/CivicrmInOperator.php
+++ modules/civicrm_views/src/Plugin/views/filter/CivicrmInOperator.php
@@ -13,8 +13,6 @@ use Drupal\civicrm\Civicrm;
 class CivicrmInOperator extends InOperator {
   public function __construct(array $configuration, $plugin_id, $plugin_definition, Civicrm $civicrm) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
-
-    $this->pseudovalues = call_user_func_array($this->definition['pseudo callback'], $this->definition['pseudo arguments']);
   }
 
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
@@ -24,14 +22,5 @@ class CivicrmInOperator extends InOperator {
       $plugin_definition,
       $container->get('civicrm')
     );
-  }
-
-  public function getValueOptions() {
-    if (isset($this->value_options)) {
-      return $this->value_options;
-    }
-
-    $this->value_options = call_user_func_array($this->definition['options callback'], $this->definition['options arguments']);
-    return $this->value_options;
   }
 }
