1de7386f4867b09fcce6e544dd996d59e51328a6
Torrance <torrance123@gmail.com>
2015-08-05 16:05:05 +1200

    Use test kernel.

--- src/Tests/CivicrmInstallation.php
+++ src/Tests/CivicrmInstallation.php
@@ -11,7 +11,7 @@ use Drupal\civicrm\Tests\CivicrmTestBase;
  */
 class CivicrmInstallation extends CivicrmTestBase {
   public function testCleanInstall() {
-    $this->assertTrue(file_exists(\Drupal\Core\DrupalKernel::getSitePath() . '/civicrm.settings.php'), "The civicrm.settings.php file was found in " .\Drupal\Core\DrupalKernel::getSitePath());
+    $this->assertTrue(file_exists($this->kernel->getSitePath() . '/civicrm.settings.php'), "The civicrm.settings.php file was found in " . $this->kernel->getSitePath());
     $this->assertTrue(function_exists('civicrm_api3'), 'civicrm_api() function exists.');
     $this->assertNotNull(\CRM_Utils_Type::BIG, "The autoloader has found the \CRM_Utils_Type class.");
   }
