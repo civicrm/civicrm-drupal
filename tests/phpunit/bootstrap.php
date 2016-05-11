6ab20c27a546638ed7b4a6710a3f6db489fac6df
Tim Otten <totten@civicrm.org>
2016-05-11 15:16:47 -0700

    CRM-14707 - tests/phpunit/bootstrap.php - Update to use `cv` bootstrap

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -1,21 +1,54 @@
 <?php
 
-if (getenv('DRUPAL_ROOT')) {
-  define('DRUPAL_ROOT', getenv('DRUPAL_ROOT'));
+// We switch between a few different container configurations during testing.
+define('CIVICRM_CONTAINER_CACHE', 'never');
+
+ini_set('memory_limit', '2G');
+ini_set('safe_mode', 0);
+eval(cv('php:boot', 'phpcode'));
+
+assert("CIVICRM_UF === 'Drupal'");
+
+/**
+ * Call the "cv" command.
+ *
+ * @param string $cmd
+ *   The rest of the command to send.
+ * @param string $decode
+ *   Ex: 'json' or 'phpcode'.
+ * @return string
+ *   Response output (if the command executed normally).
+ * @throws \RuntimeException
+ *   If the command terminates abnormally.
+ */
+function cv($cmd, $decode = 'json') {
+  $cmd = 'cv ' . $cmd;
+  $descriptorSpec = array(0 => array("pipe", "r"), 1 => array("pipe", "w"), 2 => STDERR);
+  $oldOutput = getenv('CV_OUTPUT');
+  putenv("CV_OUTPUT=json");
+  $process = proc_open($cmd, $descriptorSpec, $pipes, __DIR__);
+  putenv("CV_OUTPUT=$oldOutput");
+  fclose($pipes[0]);
+  $result = stream_get_contents($pipes[1]);
+  fclose($pipes[1]);
+  if (proc_close($process) !== 0) {
+    throw new RuntimeException("Command failed ($cmd):\n$result");
+  }
+  switch ($decode) {
+    case 'raw':
+      return $result;
+
+    case 'phpcode':
+      // If the last output is /*PHPCODE*/, then we managed to complete execution.
+      if (substr(trim($result), 0, 12) !== "/*BEGINPHP*/" || substr(trim($result), -10) !== "/*ENDPHP*/") {
+        throw new \RuntimeException("Command failed ($cmd):\n$result");
+      }
+      return $result;
+
+    case 'json':
+      return json_decode($result, 1);
+
+    default:
+      throw new RuntimeException("Bad decoder format ($decode)");
+  }
 }
-else {
-  // drupal/sites/all/modules/civicrm/drupal/tests/phpunit
-  define('DRUPAL_ROOT', dirname(dirname(dirname(dirname(dirname(dirname(dirname(realpath(__DIR__)))))))));
-}
-
-// Argh.  Crib from _drush_bootstrap_drupal_site_validate
-$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
-
-chdir(DRUPAL_ROOT);
-require_once('includes/bootstrap.inc');
-drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-
-// Drupal just usurped PHPUnit's error handler.  Kick it off the throne.
-restore_error_handler();
-
-civicrm_initialize();
