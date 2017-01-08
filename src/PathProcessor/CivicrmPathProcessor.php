f4b87adfdec205ded3398b9a291bf29b77d306aa
Mark Hanna <mark@skvare.com>
2017-01-07 18:25:47 -0600

    better handling of paths

--- src/PathProcessor/CivicrmPathProcessor.php
+++ src/PathProcessor/CivicrmPathProcessor.php
@@ -12,20 +12,34 @@ class CivicrmPathProcessor implements InboundPathProcessorInterface {
   public function processInbound($path, Request $request) {
     // if the path is a civicrm path   
     if (strpos($path, '/civicrm/') === 0) {
+      
       // initialize civicrm
       $civicrm = new Civicrm();
       // fetch civicrm menu items
       $items = \CRM_Core_Menu::items();
+      $longest = '';
       foreach (array_keys($items) as $item) {
+        $item = '/' . $item;
         // if he current path is a civicrm path
-        if(('/' . $item  . '/') == $path) {
+        if ((strpos($path, $item ) === 0))  {
           // discover any additional path components after the registered route
-          $regex_path = str_replace('/', '\/', $path);
-          $params = preg_replace("|^$regex_path|", '', $path);
-          // replace slashes with colons and the controller will piece it back together
-          $params = str_replace('/', ':', $params);
-          return "$path/$params";
+          
+          if (strlen($item) > strlen($longest)) {
+           $longest = $item;
+          }
         }
+      }
+      if (!empty($longest)) {
+       
+        $regex_path = '|^' . str_replace('/', '\/', $longest) . '\/|';
+        $params = preg_replace($regex_path, '', $path);
+         
+          // replace slashes with colons and the controller will piece it back together
+        if(strlen($params)) {
+          $params = str_replace('/', ':', $params);
+        }
+        $new_path = "$longest/$params";
+        return $new_path;     
       }
     }
     return $path;
